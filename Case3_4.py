1669138dfc4487da7863fe07b628eeceadbfa3f0
Milana Arustamyan <92077087+suphrt@users.noreply.github.com>
2021-11-30 09:37:35 +0300

    Update Case3_4.py

--- Case3_4.py
+++ Case3_4.py
@@ -1,23 +1,25 @@
 #!usr/bin/env python3
 
-from math import sqrt
+if __name__ == "__main__":
 
-n = int(input("Enter number \n"))
+    from math import sqrt
 
-if n < 2:
-    print("The number must be more than 1")
-    quit()
-elif n == 2:
-    print("1")
-    quit()
+    n = int(input("Enter number \n"))
 
-i = 2
-
-k = int(sqrt(n))
-
-while i <= k:
-    if n % i == 0:
-        print("0")
+    if n < 2:
+        print("The number must be more than 1")
         quit()
-    i += 1
-print("1")
+    elif n == 2:
+        print("1")
+        quit()
+
+    i = 2
+
+    k = int(sqrt(n))
+
+    while i <= k:
+        if n % i == 0:
+            print("0")
+            quit()
+        i += 1
+    print("1")
