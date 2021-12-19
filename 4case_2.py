97d82b40ef9e78b3c43a93e2d64db18f3d931e77
Milana Arustamyan <92077087+suphrt@users.noreply.github.com>
2021-12-19 15:27:09 +0300

    Update 4case_2.py

--- 4case_2.py
+++ 4case_2.py
@@ -1,8 +1,10 @@
 # !/usr/bin/env python3
 # /-*- coding: UTF-8 -*-
 
+from math import prod
+
 if __name__ == "__main__":
-      from math import prod
+
       lst = list(map(int, input().split()))
       lst = prod([int(a) for a in lst if a > 0])
       print(lst)
